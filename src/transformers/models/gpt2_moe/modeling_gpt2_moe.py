935ad96a1dc159f1fcf252b07313e49d1c75bb2f
shorouq.zahra <shorouq.zahra@ri.se>
2025-11-22 09:36:35 +0100

    fix output with pass

--- src/transformers/models/gpt2_moe/modeling_gpt2_moe.py
+++ src/transformers/models/gpt2_moe/modeling_gpt2_moe.py
@@ -962,7 +962,7 @@ class GPT2MoEForCausalLM(GPT2MoEPreTrainedModel, GenerationMixin):
             output_router_logits=output_router_logits,
             use_cache=use_cache,
         )
-        hidden_states = transformer_outputs[0]
+        hidden_states = transformer_outputs.last_hidden_state
 
         lm_logits = self.lm_head(hidden_states)
 
@@ -993,6 +993,8 @@ class GPT2MoEForCausalLM(GPT2MoEPreTrainedModel, GenerationMixin):
             aux_loss=aux_loss,
             logits=lm_logits,
             past_key_values=transformer_outputs.past_key_values,
+            hidden_states=transformer_outputs.hidden_states,
+            attentions=transformer_outputs.attentions,
             router_logits=transformer_outputs.router_logits,
         )
 
